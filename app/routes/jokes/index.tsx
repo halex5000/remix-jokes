aa9d8a12d18c193d6ea714a4d111e51c0e3b0c00
Alexander Hardman <alexander.hardman@gmail.com>
2022-12-27 15:08:49 -0500

    serving random joke on loading /jokes

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,11 +1,31 @@
+import { Link, useLoaderData } from '@remix-run/react';
+import { json } from 'react-router';
+import {db} from '~/utils/db.server'
+
+export const loader = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  console.log('random row number:', randomRowNumber);
+  
+  const [joke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber
+  })
+
+  console.log('random joke', joke);
+
+  return json({
+    joke
+  })
+}
+
 export default function JokesIndexRoute() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div>
       <p>Here's a random joke:</p>
-      <p>
-        I was wondering why the frisbee was getting bigger,
-        then it hit me.
-      </p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} Permalink</Link>
     </div>
   );
 }
--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,51 +1,71 @@
-const jokes = [
+const jokes: Joke[] = [
   {
-    id: "1",
+    id: 1,
     name: "Road worker",
     content: `I never wanted to believe that my Dad was stealing from his job as a road worker. But when I got home, all the signs were there.`,
   },
   {
-    id: "2",
+    id: 2,
     name: "Frisbee",
     content: `I was wondering why the frisbee was getting bigger, then it hit me.`,
   },
   {
-    id: "3",
+    id: 3,
     name: "Trees",
     content: `Why do trees seem suspicious on sunny days? Dunno, they're just a bit shady.`,
   },
   {
-    id: "4",
+    id: 4,
     name: "Skeletons",
     content: `Why don't skeletons ride roller coasters? They don't have the stomach for it.`,
   },
   {
-    id: "5",
+    id: 5,
     name: "Hippos",
     content: `Why don't you find hippopotamuses hiding in trees? They're really good at it.`,
   },
   {
-    id: "6",
+    id: 6,
     name: "Dinner",
     content: `What did one plate say to the other plate? Dinner is on me!`,
   },
   {
-    id: "7",
+    id: 7,
     name: "Elevator",
     content: `My first time using an elevator was an uplifting experience. The second time let me down.`,
   },
 ];
 
-type Params = {
-  where: { id: string },
+type FindUniqueParams = {
+  where: { id?: number },
+}
+
+type FindManyParams = {
+  take?: number;
+  skip?: number;
+}
+
+type Joke = {
+  id: number;
+  content: string;
+  name: string;
 }
 
 const db = {
   joke: {
-    async findMany() {
-      return jokes;
+    async count(): Promise<number> {
+      return jokes.length;
     },
-    async findUnique({where: {id}}: Params) {
+    async findMany(params?: FindManyParams): Promise<Joke[]> {
+      if (!params) return jokes;
+      const id = params.skip;
+      if (id) {
+        const joke = jokes.find(joke => joke.id === id)
+        if (joke) return [joke]
+      }
+      return [];
+    },
+    async findUnique({where: {id}}: FindUniqueParams): Promise<Joke | undefined> {
       return jokes.find(joke => joke.id === id)
     }
   }
